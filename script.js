58d99f7c60062da5a90c831a2a5901f57e79ecf2
Thameem-Ali0911 <cpthameemali@gmail.com>
2025-04-18 16:52:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function login(){
+	if (document.getElementById('email').value==='123@123.com' && document.getElementById('password').value==='123') {
+        window.location.href='https://github.com/Thameem-Ali0911';
+        } else {
+              alert('Please Enter Correct Username and Password');
+               }
+        }
+function signin(){
+	alert('Sign In successful');
+	}   
